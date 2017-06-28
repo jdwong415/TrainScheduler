2578136173a60428d0953cf6047b634c4d917753
Jonathan Wong <jdwong2@yahoo.com>
2017-06-27 23:55:05 -0700

    Add time calculations and remove train

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,15 +16,27 @@ var name;
 var destination;
 var time;
 var frequency;
+var minutesAway;
+var nextTime;
 
-function createRow() {
-  var tr = $("<tr>");
+function createRow(key) {
+  var tr = $("<tr id='" + key + "'>");
   tr.append($("<td>").text(name));
   tr.append($("<td>").text(destination));
   tr.append($("<td>").text(frequency));
-  tr.append($("<td>").text("NEXT TIME"));
-  tr.append($("<td>").text("MINUTES AWAY"));
+  tr.append($("<td>").text(nextTime));
+  tr.append($("<td>").text(minutesAway));
+  var close = "<button type='button' class='close' aria-label='Close'>" +
+    "<span aria-hidden='true'>&times;</span></button>";
+  tr.append($("<td>").html(close));
   $("#train-info").append(tr);
+}
+
+function calculateTime() {
+  var convertedTime = moment(time, "HH:mm");
+  var diffTime = moment().diff(convertedTime, "minutes");
+  minutesAway = frequency - (diffTime % frequency);
+  nextTime = moment().add(minutesAway, "minutes").format("hh:mm a");
 }
 
 dataRef.on("child_added", function(snapshot) {
@@ -34,7 +46,9 @@ dataRef.on("child_added", function(snapshot) {
   time = snapshot.val().time;
   frequency = snapshot.val().frequency;
 
-  createRow();
+  calculateTime();
+
+  createRow(snapshot.key);
 
 });
 
@@ -60,3 +74,10 @@ $("#add-train").on("click", function() {
 
 });
 
+$(document).on("click", ".close", function() {
+  console.log($(this).parent().parent().attr("id"));
+  var key = $(this).parent().parent().attr("id");
+  dataRef.child(key).remove();
+  $(this).parent().parent().remove();
+
+});
